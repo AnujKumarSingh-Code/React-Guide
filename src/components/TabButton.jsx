92dbc45b15b3c426c3f6348ae4393316a4d561fa
Anuj Kumar <anujkumarcoder@gmail.com>
2024-10-28 00:14:01 +0530

    Conditional CSS

--- src/components/TabButton.jsx
+++ src/components/TabButton.jsx
@@ -2,7 +2,7 @@ export default function TabButton(props) {
 
     return (
         <l1>
-            <button onClick={props.onSelect}>
+            <button className = {props.isSelected ? 'active' : undefined} onClick={props.onSelect}>
                 {props.children}
             </button>
         </l1>
